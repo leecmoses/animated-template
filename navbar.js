4f0d8b02489a975b23446ab263dcab56ff64e993
Moses Lee <leecmoses@gmail.com>
2022-06-05 11:55:12 -0400

    Refactor

---
+++ navbar.js
@@ -0,0 +1,6 @@
+"use strict";
+
+function toggleNavbar(collapseID) {
+  document.getElementById(collapseID).classList.toggle("hidden");
+  document.getElementById(collapseID).classList.toggle("block");
+}
